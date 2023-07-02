e44899a6a61f589561ced26e0d74db19bf7aee81
vomi-exe <mehtavomi@hgmail.com>
2023-07-02 17:29:57 +0530

    now you can see posts :)

---
+++ src/components/SinglePost.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="singlePost">
+        <h3>
+            {{ post.title }}
+        </h3>
+        <p>{{ snippet }}</p>
+    </div>
+</template>
+
+<script>
+import { computed, onMounted } from 'vue';
+
+export default {
+    name: "SinglePost",
+    props: ['post'],
+    setup(props) {
+        const snippet = computed(() => {
+            return props.post.body.substring(0, 100) + "...";
+        })
+
+        return { snippet };
+
+
+    }
+}
+
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Home</h1>
+    <div v-if="error" style="color: red">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else style="color: green">Loading...</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import { ref } from 'vue';
+import PostList from "../components/PostList.vue";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name: "About",
+  components: { PostList },
+  setup() {
+    const posts = ref([])
+    const error = ref(null);
+
+    const load = async () => {
+      try {
+        let data = await fetch("http://localhost:3000/posts");
+        if (!data.ok) {
+          throw Error("no data available");
+        }
+        posts.value = await data.json();
+
+      } catch (err) {
+        error.value = err.message;
+        console.log(err.value);
+      }
+    }
+    load();
+
+    return { posts, error }
   }
 }
 </script>
+
+<style></style>
